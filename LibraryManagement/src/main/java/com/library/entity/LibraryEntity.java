24dce9a2bc5c6f96b097a26cc9975e96d9f4e319
Naveed <navid.nony@gmail.com>
2024-06-01 01:45:10 +0530

    Created an entity class

---
+++ LibraryManagement/src/main/java/com/library/entity/LibraryEntity.java
@@ -0,0 +1,45 @@
+package com.library.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Library_Table")
+public class LibraryEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long book_id;
+	@Column
+	private String book_name;
+	private String book_author;
+
+	public Long getBook_id() {
+		return book_id;
+	}
+
+	public void setBook_id(Long book_id) {
+		this.book_id = book_id;
+	}
+
+	public String getBook_name() {
+		return book_name;
+	}
+
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+
+	public String getBook_author() {
+		return book_author;
+	}
+
+	public void setBook_author(String book_author) {
+		this.book_author = book_author;
+	}
+
+}
