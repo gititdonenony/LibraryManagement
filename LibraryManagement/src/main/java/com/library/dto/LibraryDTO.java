26b88d3edd666bb5ccc93c29e231329096e65ad5
Naveed <navid.nony@gmail.com>
2024-06-01 01:48:56 +0530

    Created a DTO class

---
+++ LibraryManagement/src/main/java/com/library/dto/LibraryDTO.java
@@ -0,0 +1,32 @@
+package com.library.dto;
+
+public class LibraryDTO {
+	private Long book_id;
+	private String book_name;
+	private String book_author;
+
+	public Long getBook_id() {
+		return book_id;
+	}
+
+	public void setBook_id(Long book_id) {
+		this.book_id = book_id;
+	}
+
+	public String getBook_name() {
+		return book_name;
+	}
+
+	public void setBook_name(String book_name) {
+		this.book_name = book_name;
+	}
+
+	public String getBook_author() {
+		return book_author;
+	}
+
+	public void setBook_author(String book_author) {
+		this.book_author = book_author;
+	}
+
+}
